5d4361039db3506878591e80b72941a447e09d9b
Tony Hu Tong <buildmindht@outlook.com>
2019-07-29 20:40:16 +0800

    Update detection fetching with interval query

--- src/api/detection.js
+++ src/api/detection.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const moment = require("moment");
 const router = express.Router();
 const Detection = require("../models/detection.js");
 
 router.get("/", async (req, res) => {
   // Get All For User
   try {
-    let interval = req.query.interval;
+    let interval = req.query.interval || "second";
     let limit = 7;
 
     if (interval === "perfiveminute") {
       limit = 7 * 5 * 30;
     } else if (interval === "hourly") {
       limit = 7 * 60 * 30;
+    } else if (interval === "daily") {
+      limit = 7 * 60 * 30 * 24;
     }
 
     const data = await Detection.findAll({
@@ -21,21 +24,67 @@ router.get("/", async (req, res) => {
       },
       order: [["createdAt", "DESC"]]
     });
-    let returnData = data.map(item => {
-      return {
-        numberOfPerson: item.numberOfPerson,
-        numberOfSmoker: item.numberOfSmoker
-      };
-    });
 
-    if (data.length < 7) {
-      for (var i = 0; i < 7 - data.length; i++) {
+    let returnData;
+    returnData = new Array(7);
+    let subtract = 1;
+    if (interval === "second") {
+      subtract = 1;
+    }
+    if (interval === "perfiveminute") {
+      subtract = 60 * 5;
+    }
+    if (interval === "hourly") {
+      subtract = 60 * 60;
+    }
+    if (interval === "daily") {
+      subtract = 60 * 60 * 24;
+    }
+    for (var i = 0; i < 7; i++) {
+      const anchor = [
+        moment().subtract((7 - i) * subtract, "second"),
+        moment().subtract((6 - i) * subtract, "second")
+      ];
+      data.forEach(detection => {
+        const isSameOrAfter = moment(detection.createdAt).isSameOrAfter(
+          anchor[0]
+        );
+        const isBefore = moment(detection.createdAt).isBefore(anchor[1]);
+        if (isSameOrAfter && isBefore) {
+          if (!returnData[i]) {
+            returnData[i] = {
+              numberOfPerson: detection.numberOfPerson,
+              numberOfSmoker: detection.numberOfSmoker
+            };
+          } else {
+            returnData[i].numberOfPerson += detection.numberOfPerson;
+            returnData[i].numberOfSmoker += detection.numberOfSmoker;
+          }
+        } else {
+          returnData[i] = {
+            numberOfPerson: 0,
+            numberOfSmoker: 0
+          };
+        }
+      });
+
+      if (!data) {
+        returnData = returnData.map(() => ({
+          numberOfPerson: 0,
+          numberOfSmoker: 0
+        }));
+      }
+    }
+
+    if (returnData.length < 7) {
+      for (var i = 0; i < 7 - returnData.length; i++) {
         returnData.unshift({
           numberOfPerson: 0,
           numberOfSmoker: 0
         });
       }
     }
+
     res.send(returnData);
   } catch (err) {
     console.log(err);
