062ee724f18c959c70371bad286aa75c590100ba
Tony Hu Tong <buildmindht@outlook.com>
2019-10-07 12:05:53 +0900

    Update body limit on url form

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 const api = require("./api/index.js");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => res.send("Hello World!"));
