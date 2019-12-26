3d3233801e6b8fcce8d02b5119fe2315be5afb03
john <john>
2019-12-26 17:14:24 +0530

    local running issue

--- src/local.js
+++ src/local.js
@@ -1,7 +1,9 @@
+const app = require('./app')
+const port = process.env.PORT || 3000;
+
 const server = require("http").createServer(app);
 server.listen(port, () => console.log(`Customindz listening on port ${port}!`));
 
-const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === "local") {
   // global.cv = require("/usr/lib/node_modules/opencv4nodejs");
