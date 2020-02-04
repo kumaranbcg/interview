116fa4125d1152ae163edc307803a1cb1a09993d
john <john>
2020-02-04 12:54:22 +0530

    test socket

--- src/io.js
+++ src/io.js
@@ -1,13 +1,34 @@
 const socketio = require("socket.io");
+
+const { Devices, ZoomConfig } = require("./lib/db");
+
+
 let io;
 module.exports = server => {
   if (!io && server) {
     io = socketio(server);
 
     io.on("connection", socket => {
-      socket.on("listen-monitor", room => {
-        console.log("Some one joined monitor " + room);
-        socket.join(room);
+      console.log(socket.id, socket.handshake.address)
+      socket.on("get-zoom-data", async id => {
+        const query = {
+          where: {
+            id
+          },
+          include: [
+            {
+              model: ZoomConfig,
+              as: "detectionZone1",
+            },
+            {
+              model: ZoomConfig,
+              as: "detectionZone2",
+            }
+          ]
+        }
+        const data = await Devices.findOne(query)
+
+        socket.emit('zoom-data', data)
       });
     });
   }
