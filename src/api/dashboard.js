479ac71d30f37aba923f6d5bf10ecdb61670c27e
john <john>
2020-02-11 12:00:58 +0530

    camera list api

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,165 +1,20 @@
 const express = require("express");
+const shortid = require("shortid");
 const router = express.Router();
+const Sequelize = require("sequelize");
 
-const { Alert } = require("../lib/db");
-const { today,yesterday } = require("../lib/utils");
+const { Alert,sequelize } = require("../lib/db");
+const { today, yesterday } = require("../lib/utils");
 
-const { Op } = require("sequelize");
+const { Op, QueryTypes } = require("sequelize");
 
-router.get('/', async (req, res) => {
+const { Projects, Detection, Monitor } = require("../lib/db");
+
+router.get('/camera-list', async (req, res) => {
   try {
 
-    const query = {
-      where: {},
-    };
+    const data = await sequelize.query("SELECT c.id, c.name, COUNT(*) as alerts FROM `monitors` c JOIN `detections` d ON c.id=d.monitor_id GROUP BY d.monitor_id", { type: QueryTypes.SELECT });
 
-    if (req.query.monitor_id) {
-      query.monitor_id = req.query.monitor_id;
-    }
-
-    const todayDate = today();
-    const yesterdayDate = yesterday();
-
-    const alertAll = await Alert.findAndCountAll({
-      ...query, where: {
-      }
-    });
-
-    const alert = await Alert.findAndCountAll({
-      ...query, where: {
-        createdAt: todayDate
-      }
-    });
-
-    const alertYesterday = await Alert.findAndCountAll({
-      ...query, where: {
-        createdAt: yesterdayDate
-      }
-    });
-
-
-    const data = {
-
-      safetyStat: [
-        {
-          title: "Alerts Today",
-          percent: { value: alertAll.count + alert.count / 2, profit: Boolean(alertYesterday.count + alert.count / 2) },
-          total: {
-            value: alertAll.count,
-          },
-          yesterday: {
-            value: alertYesterday.count, profit: false
-          },
-          average: {
-            value: alertYesterday.count + alert.count / 2, profit: false
-          },
-          options: [
-            {
-              text: "Daily",
-              value: "daily"
-            },
-            {
-              text: "Weekly",
-              value: "weekly"
-            },
-            {
-              text: "Monthly",
-              value: "monthly"
-            }
-          ],
-          chartColor: "#2b518e"
-        },
-        {
-          title: "Safety Level",
-          percent: { value: 0, profit: false },
-          total: {
-            value: alert.count,
-          },
-          yesterday: {
-            value: alertYesterday.count, profit: false
-          },
-          average: {
-            value: 0, profit: false
-          },
-          chartColor: "#ffa630"
-        },
-        {
-          title: "Savings on Monitoring",
-          percent: { value: 0, profit: false },
-
-          total: {
-            value: alert.count,
-          },
-          yesterday: {
-            value: alertYesterday.count, profit: false
-          },
-          average: {
-            value: 0, profit: false
-          },
-          background: "#ffa630",
-          color: "#fff",
-          chartColor: "#FFF"
-        }
-      ],
-      savingsStat: [
-        {
-          title: "Minutes Monitored",
-          percent: { value: 0, profit: false },
-
-          total: {
-            value: alert.count,
-          },
-          yesterday: {
-            value: alertYesterday.count, profit: false
-          },
-          average: {
-            value: 0, profit: false
-          },
-          options: [
-            {
-              text: "Daily",
-              value: "daily"
-            },
-            {
-              text: "Weekly",
-              value: "weekly"
-            },
-            {
-              text: "Monthly",
-              value: "monthly"
-            }
-          ],
-          chartColor: "#2b518e"
-        },
-        {
-          title: "Best Day", percent: { value: 0, profit: false },
-
-          total: {
-            value: alert.count,
-          },
-          yesterday: {
-            value: alertYesterday.count, profit: false
-          },
-          average: {
-            value: 0, profit: false
-          },
-          chartColor: "#2b518e"
-        },
-        {
-          title: "Worst Day", percent: { value: 0, profit: false },
-          total: {
-            value: alert.count,
-          },
-          yesterday: {
-            value: alertYesterday.count, profit: false
-          },
-          average: {
-            value: 0, profit: false
-          },
-          chartColor: "#2b518e"
-        }
-      ],
-    }
     res
       .send(data)
       .status(200)
