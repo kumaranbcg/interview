52fd0a94310b8370052b05d77c727992972ef67d
john <john>
2020-02-18 16:13:34 +0530

    video

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -555,11 +555,11 @@ router.get('/detections', async (req, res) => {
   try {
 
 
-    const { period_from = moment().format(DATE_FORMAT), period_to = moment().format(DATE_FORMAT), engine = 'danger-zone' } = req.query;
+    const { period_from = moment().format(DATE_FORMAT), period_to = moment().format(DATE_FORMAT), engine = 'danger-zone', monitor_id } = req.query;
 
-    const detections = await sequelize.query("SELECT a.id,a.name,a.machine_id,a.device_id,a.ip,a.config, b.* FROM `monitors` a RIGHT JOIN `detections` b ON a.id= b.monitor_id WHERE engine=:engine AND s.created_at BETWEEN :period_from AND :period_to ORDER BY s.created_at DESC",
+    const detections = await sequelize.query("SELECT a.id,a.name,a.machine_id,a.device_id,a.ip,a.config, b.* FROM `monitors` a RIGHT JOIN `detections` b ON a.id= b.monitor_id WHERE b.monitor_id = :monitor_id AND engine=:engine AND b.created_at BETWEEN :period_from AND :period_to ORDER BY b.created_at DESC",
       {
-        replacements: { period_from, period_to, engine },
+        replacements: { period_from, period_to, engine, monitor_id },
         type: QueryTypes.SELECT
       });
 
