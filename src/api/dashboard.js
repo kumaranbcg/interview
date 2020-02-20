23a8e7725691f7cc84a74e8295adec1fbf4641b3
john <john>
2020-02-20 11:10:13 +0530

    update

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -415,7 +415,8 @@ router.get('/progress', async (req, res) => {
 
     const completedPercentage = Number(target ? totalRemoved / target * 100 : totalRemoved).toFixed(0);
 
-    const trucksDailyAverage = trucksTotal / detectionsByDate.length || 1;
+    // const trucksDailyAverage = trucksTotal / detectionsByDate.length || 1;
+    const trucksDailyAverage = detectionsByDate.length ? trucksTotal / detectionsByDate.length : trucksTotal;
 
     const dailyAverageRemoved = trucksDailyAverage * capacity;
 
