cce265c17a96e77dd6a7e0668ba2990dbae95d62
john <john>
2020-02-24 15:29:14 +0530

    update code

--- src/test/socket.js
+++ src/test/socket.js
@@ -1,20 +1,24 @@
-const URL = 'http://localhost:3000';
+const URL = 'http://3.20.122.193';
 var socket = require('socket.io-client')(URL);
 const axios = require('axios').default;
 socket.on('connect', async () => {
+  console.log('connected')
   // Initialize Monitor ID 
-  const monitor_id = 'jetson_camera_test'
+  const monitor_id = 'jetson_camera_test123'
   // Take Socket ID and store for passing in detection
   const socket_id = socket.id;
 
   setTimeout(async () => {
     socket.emit('send-meta', {
-      monitor_id: 'jetson_camera_test'
+      monitor_id
     })
+    console.log('sent-meta')
+
   }, 1000)
 
 
   setTimeout(async () => {
+    console.log('disconnect')
     socket.disconnect();
     // If any detection found send the video url or image url with socket id information and engine type along with camera id
     await axios.post(`${URL}/api/admin/detection/incoming`, {
@@ -24,51 +28,7 @@ socket.on('connect', async () => {
       "video_url": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
       "image_url": "https://media.gettyimages.com/photos/high-angle-view-of-people-on-street-picture-id973190966?s=2048x2048"
     });
-  }, process.env.TIMEOUT || 320000)
-
-  // socket.emit('get-device', { id: 1 });
-  // socket.emit('change-zoom', {
-  //   id: 2,
-  //   config: {
-  //     "c1p1x1": "0.10",
-  //     "c1p1x2": "1",
-  //     "c1p1x3": "1",
-  //     "c1p1x4": "0",
-  //     "c1p1y1": "1",
-  //     "c1p1y2": "1",
-  //     "c1p1y3": "0.45",
-  //     "c1p1y4": "",
-  //     "c1p2x1": "0.09",
-  //     "c1p2x2": "1",
-  //     "c1p2x3": "1",
-  //     "c1p2x4": "0",
-  //     "c1p2y1": "1",
-  //     "c1p2y2": "1",
-  //     "c1p2y3": "0.45",
-  //     "c1p2y4": "",
-  //     "c2p1x1": "0.09",
-  //     "c2p1x2": "1",
-  //     "c2p1x3": "1",
-  //     "c2p1x4": "0",
-  //     "c2p1y1": "1",
-  //     "c2p1y2": "1",
-  //     "c2p1y3": "0.45",
-  //     "c2p1y4": "",
-  //     "c2p2x1": "0.09",
-  //     "c2p2x2": "1",
-  //     "c2p2x3": "1",
-  //     "c2p2x4": "0",
-  //     "c2p2y1": "1",
-  //     "c2p2y2": "1",
-  //     "c2p2y3": "0.45",
-  //     "c2p2y4": ""
-  //   }
-  // });
-  // socket.emit('update-device', {
-  //   id: 2,
-  //   order: 1,
-  //   config: 1
-  // });
+  }, process.env.TIMEOUT || 60000)
 
 });
 
