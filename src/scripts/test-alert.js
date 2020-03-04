ddf589d92c75ae38683f04e67a2137b6f780e316
Donald <donaldz0728@gmail.com>
2020-03-04 14:31:27 +0800

    fix

--- src/scripts/test-alert.js
+++ src/scripts/test-alert.js
@@ -1,20 +1,49 @@
 const alert = require("../lib/alert");
 
+// alert.do(
+//   [
+//     {
+//       id: "8b8901b2-b58d-4823-8bd1-a88f0d2b0f79",
+//       engine: "dump-truck",
+//       interval: 0,
+//       output_type: "Email",
+//       // output_address:
+//       //   "gary.ng@customindz.com,harry.ng@dixlpm.com.hk,buildmindht@outlook.com,izaac.leung@customindz.com,hc@botzup.com,jurge92@icloud.com,zq.donald.chong@gmail.com",
+//       // output_address: "buildmindht@outlook.com",
+//       output_address:"zq.donald.chong@gmail.com,Joergen@viact.ai",
+//       trigger_record: false,
+//       alert_type: "Trigger",
+//       created_at: "2020-03-13 18:37:13",
+//       updatedAt: "2020-03-03T18:37:13.000Z",
+//       monitor_id: "tuspark-roof",
+//       monitor:{
+//         name: "HHDT1"
+//       }
+//     }
+//   ],
+//   {
+//     image: `https://windht.github.io/customindz-front-end-react-app/static/media/new_logo.a27f7193.png`,
+//     url: "http://http://hhdt1.viact.ai/#/user/alert-detail/8b8901b2-b58d-4823-8bd1-a88f0d2b0f79",
+//     title: "Alert for HHDT1",
+//     subtitle: "Check the alert"
+//   }
+// );
+
 alert.do(
   [
     {
-      id: "4df8a824-f5d4-4004-a7af-89fb9da3e05f",
+      id: "8b8901b2-b58d-4823-8bd1-a88f0d2b0f79",
       engine: "dump-truck",
       interval: 0,
-      output_type: "Email",
+      output_type: "SMS",
       // output_address:
       //   "gary.ng@customindz.com,harry.ng@dixlpm.com.hk,buildmindht@outlook.com,izaac.leung@customindz.com,hc@botzup.com,jurge92@icloud.com,zq.donald.chong@gmail.com",
       // output_address: "buildmindht@outlook.com",
-      output_address:"zq.donald.chong@gmail.com,donaldz0728@gmail.com,34400765@qq.com",
+      output_address:"+85259231994",
       trigger_record: false,
       alert_type: "Trigger",
-      created_at: "2020-02-17 12:00",
-      updatedAt: "2019-09-17T12:22:30.000Z",
+      created_at: "2020-03-13 18:37:13",
+      updatedAt: "2020-03-03T18:37:13.000Z",
       monitor_id: "tuspark-roof",
       monitor:{
         name: "HHDT1"
@@ -23,8 +52,8 @@ alert.do(
   ],
   {
     image: `https://windht.github.io/customindz-front-end-react-app/static/media/new_logo.a27f7193.png`,
-    url: "http://hhdt1.viact.ai/#/user/alert-detail/24bb3dbc-f172-488c-b04e-f0c2319b799b",
-    title: "Daily Report",
-    subtitle: "Check the daily report"
+    url: "http://http://hhdt1.viact.ai/#/user/alert-detail/8b8901b2-b58d-4823-8bd1-a88f0d2b0f79",
+    title: "Alert for HHDT1",
+    subtitle: "Check the alert"
   }
 );
