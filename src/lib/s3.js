4a64325d29a92dfcf6dc5ecf5980d86493c76114
Donald <donaldz0728@gmail.com>
2020-03-05 14:36:54 +0800

    Update s3.js

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -4,8 +4,8 @@ const AWS = require("aws-sdk");
 const spacesEndpoint = new AWS.Endpoint("sgp1.digitaloceanspaces.com");
 const s3 = new AWS.S3({
   endpoint: spacesEndpoint,
-  accessKeyId: "FKGBHQBS7MVZGBOHVF27",
-  secretAccessKey: "ZVMJ1kwSZRAD+H5d5uXasE2iludw8i0VLKqrzto+HMw"
+  accessKeyId: "GY4GRBWJ5HJYUZRSMYHA",
+  secretAccessKey: "J6ta27Cp8t9GO+hiJcz0EmSdqEfbYDmuPmhaEAs44Po"
 });
 
 module.exports = s3;
